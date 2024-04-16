4fd99b4d52b500e37931ddd90e2168a4fec2e257
Ju <c20chin@gmail.com>
2024-04-16 18:24:11 +0100

    Install bootstrap

--- restaurant-finder/src/App.js
+++ restaurant-finder/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Container from "react-bootstrap/Container";
+import {
+  Card,
+  InputGroup,
+  Button,
+  FormControl,
+  Row
+} from "react-bootstrap";
+import { useState, useEffect } from 'react';
+
 
 function App() {
+  const [searchInput, setSearchInput] = useState("");
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <InputGroup className="mb-3" size="lg">
+          <FormControl
+            placeholder="Enter Postcode: eg. N7 8FB"
+            type="input"
+            onChange={() => {
+              console.log("key change");
+            }}
+          />
+          <Button
+            onClick={(event) => {
+              console.log("search btn");
+            }}
+          >
+            Search
+          </Button>
+        </InputGroup>
+      </Container>
+      <Container>
+        <Row className="mx-2 row row-col-4">
+          <Card>
+            <Card.Body>
+              <Card.Title>RestaurantName</Card.Title>
+            </Card.Body>
+          </Card>
+        </Row>
+      </Container>
     </div>
   );
 }

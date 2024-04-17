23933d667d8316ff0f9bdf2830400f6ccf298d7a
Ju <c20chin@gmail.com>
2024-04-17 17:23:04 +0100

    Refine frontend margin

--- restaurant-finder/src/App.js
+++ restaurant-finder/src/App.js
@@ -8,13 +8,21 @@ import {
   FormControl,
   Row,
   Image,
-  Col
+  Col,
 } from "react-bootstrap";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function App() {
   const [searchInput, setSearchInput] = useState("");
   const [restaurants, setRestaurants] = useState([]);
+  const resultsRef = useRef(null);
+
+  useEffect(() => {
+    // Scroll to the results container when searchData changes
+    if (resultsRef.current) {
+      resultsRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  }, [restaurants]);
 
   // Search for restaurant
   async function searchRestaurant() {
@@ -40,51 +48,66 @@ function App() {
 
   return (
     <div className="App">
-      <Container>
-        <Image height="300px" src={require("./img/JetLogo.png")} fluid />
-        <p>Find your next meal by typing in your postcode!</p>
-        <InputGroup className="p-5 mb-3" size="md">
-          <FormControl
-            placeholder="Enter Postcode: eg. N7 8FB"
-            type="input"
-            onKeyDown={(event) => {
-              if (event.key == "Enter") {
-                searchRestaurant();
-              }
-            }}
-            onChange={(event) => setSearchInput(event.target.value)}
-          />
-          <Button onClick={searchRestaurant}>Search</Button>
-        </InputGroup>
+      <Container className="container-custom">
+        <Image
+          className="px-5 mx-0"
+          width="70%"
+          src={require("./img/JetLogo.png")}
+        />
+        <h4>Type in your postcode to find your next meal!</h4>
       </Container>
-      <Container>
-        <Row xs={2} md={4} className="g-4">
-          {restaurants.map((restaurant, i) => {
-            return (
-              <Col key="i">
-                <Card height="300pt">
-                  <Card.Img variant="top" width="150px" src={restaurant.logoUrl} />
-                  <Card.Body>
-                    <Card.Title>{restaurant.name}</Card.Title>
-                    <Card.Text>
-                      <img width="15px" src={require("./img/star.png")}></img>
-                      {restaurant.rating.starRating}
-                    </Card.Text>
-                    <Card.Text>{restaurant.address.firstLine}</Card.Text>
-                    {restaurant.cuisines.slice(0, 2).map((cuisine, i) => {
-                      return (
-                        <Button className="m-1" size="sm">
-                          {cuisine.name}
-                        </Button>
-                      );
-                    })}
-                  </Card.Body>
-                </Card>
-              </Col>
-            );
-          })}
-        </Row>
-      </Container>
+      <div>
+        <Container>
+          <InputGroup width="auto" className="p-5 mb-3" size="lg">
+            <FormControl
+              placeholder="Enter Postcode: eg. N7 8FB"
+              type="input"
+              onKeyDown={(event) => {
+                if (event.key === "Enter") {
+                  searchRestaurant();
+                }
+              }}
+              onChange={(event) => setSearchInput(event.target.value)}
+            />
+            <Button onClick={searchRestaurant}>Search</Button>
+          </InputGroup>
+        </Container>
+        <Container ref={resultsRef}>
+          <Row xs={2} md={4} className="g-4">
+            {restaurants.map((restaurant, i) => {
+              return (
+                <Col key="i">
+                  <Card height="150pt">
+                    <Card.Img
+                      variant="top"
+                      width="50%"
+                      className="p-5"
+                      src={restaurant.logoUrl}
+                    />
+                    <Card.Body className="card-body-custom">
+                      <Card.Title>{restaurant.name}</Card.Title>
+                      <Card.Text>
+                        <img width="15px" src={require("./img/star.png")}></img>
+                        {restaurant.rating.starRating}
+                      </Card.Text>
+                      <Card.Text className="text-sm">
+                        {restaurant.address.firstLine}
+                      </Card.Text>
+                      {restaurant.cuisines.slice(0, 2).map((cuisine, i) => {
+                        return (
+                          <Button className="m-1 btn-outline-success" size="sm" disabled>
+                            {cuisine.name}
+                          </Button>
+                        );
+                      })}
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 }
